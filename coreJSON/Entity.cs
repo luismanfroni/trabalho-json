8ad8eab6e148dc229166d7e68e3307cc89fb611f
Luis Felipe Manfroni <siul@debian.debian>
2019-03-13 19:24:09 -0300

    netCore

---
+++ coreJSON/Entity.cs
@@ -0,0 +1,11 @@
+namespace coreJSON
+{
+	public class Pedido 
+	{
+		public string nome;
+		public string endereco;
+		public string pedido;
+		public string restaurante;
+		public string telefone;
+	}
+}
---
+++ coreJSON/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace coreJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pedido pedidoImportado;
+            try 
+            {
+                pedidoImportado = importPedido("gravando.json");
+            } catch(FileNotFoundException ex){
+                pedidoImportado = new Pedido(){
+                    nome = "Usuario Teste",
+                    endereco = "Joinville",
+                    pedido = "Pizza Grande",
+                    restaurante = "Baggio",
+                    telefone = "1234 5678"
+                };
+            }
+			exportPedido(pedidoImportado, "gravando.json");
+        }
+
+        private static Pedido importPedido(string arquivo)
+        {
+        	string json = File.ReadAllText(arquivo);
+        	return JsonConvert.DeserializeObject<Pedido>(json);
+        }
+
+        private static void exportPedido(Pedido pedido, string arquivo)
+        {
+        	string json = JsonConvert.SerializeObject(pedido);
+        	File.WriteAllText(arquivo, json);
+        }
+    }
+}
